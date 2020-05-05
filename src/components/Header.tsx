4915e3fb75a7a62549820cc069d7f7874b018ef9
prostiak2019 <yarovenkodmv@gmail.com>
2020-05-05 19:00:12 +0300

    SocialNetwork_v-12

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header className={'header'}>
+      <img
+        src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmZJl9r7JapjoydUuxwLEWyGBSJqFNwoBfRGlnZYdXzd-YoXY2Yo2ylJnjng&s'/>
+    </header>
+  )
+};
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <nav className={'nav'}>
+      <div>
+        <a>TS Profile TS</a>
+      </div>
+      <div>
+        <a>TS Message TS</a>
+      </div>
+      <div>
+        <a>TS News TS</a>
+      </div>
+      <div>
+        <a>TS Music TS</a>
+      </div>
+      <div>
+        <a>TS Settings TS</a>
+      </div>
+    </nav>
+
+  )
+};
