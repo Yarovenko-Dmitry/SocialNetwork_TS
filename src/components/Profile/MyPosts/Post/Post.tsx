45367e65fa72a366212685347eb78c1e71552a36
prostiak2019 <yarovenkodmv@gmail.com>
2020-05-06 17:58:50 +0300

    SocialNetwork_v-16-TS

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import s from './Post.module.css'
 
-const Post = () => {
+type PostType = {
+  message: string
+}
+
+const Post = (props: PostType) => {
   return (
     <div className={s.item}>
       <img src={'https://i.pinimg.com/originals/63/2d/6f/632d6f00a809cf8c77f65142395458de.png'}/>
-      post1
+      {props.message}
       <div>
         <span>like</span>
       </div>
